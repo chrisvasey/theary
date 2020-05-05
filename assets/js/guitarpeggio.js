d26743a42dd6e715fe88a7fa139b346d98a57169
Chris Vasey <chrismvasey@gmail.com>
2020-05-05 22:51:38 +0100

    Added the ability to generate multiple arpeggios

--- assets/js/guitarpeggio.js
+++ assets/js/guitarpeggio.js
@@ -6,7 +6,7 @@
 /*
     Here we declare our options
 */
-var direction = ['an ascending', 'a descending'];
+var direction = ['an ascending <i class="fa fa-arrow-up"></i>', 'a descending <i class="fa fa-arrow-down"></i>'];
 var note = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#'];
 var chordType = ['maj7', 'min7', '7'];
 var interval = ['1st', '3rd', '5th', '7th'];
@@ -30,10 +30,51 @@ function generateArp() {
     var output = "Play " + randomItem(direction) + " <code>" + randomItem(note) + randomItem(chordType) + "</code> arpeggio starting from the <code>" + randomItem(interval) + "</code> on the <code>" + randomItem(guitarString) + "</code> string.";
     //This is used to output our new string to the <h3> title tag witht he id of "output"
     $('#output').html(output);
+
+    //Increment the amount completed
+    amountCompleted++;
+
+    //Check to see if they have met their goal
+    if (amountCompleted == (parseInt(goalArpeggio) + 1)) {
+        goalComplete();
+    } else {
+        if (amountCompleted == goalArpeggio) {
+            $('#nextArp').text('Complete Arpeggio');
+        }
+        $('#amountCompleted').text(amountCompleted);
+    }
 }
 
-/*
-    This will run when the page loads to create our first arpeggio.
-    We will also fire thie when the "New Arpeggio" button is pressed.
-*/
-generateArp();
+var goalArpeggio = null;
+var amountCompleted = 0;
+
+function setupArpeggio() {
+    //Get value from the arp amount input box, then hide it
+    goalArpeggio = $('#arpInput').val();
+
+    //Check for input
+    if (goalArpeggio == '' || goalArpeggio == 0) {
+        return alert('Please enter a number!')
+    }
+
+    $('#arpInput').toggleClass('hidden');
+    $('#genArp').toggleClass('hidden');
+
+    //Set up 0/22 output
+    $('#maxAmount').text(goalArpeggio);
+    $('#completedOutput').toggleClass('hidden');
+
+
+    //Show the button for next arp
+    $('#nextArp').toggleClass('hidden');
+
+    //Generate first arp
+    generateArp();
+}
+
+function goalComplete() {
+    $('#output').html('Well done! Now do another ' + goalArpeggio);
+    $('#startAgain').toggleClass('hidden');
+    $('#nextArp').toggleClass('hidden');
+    $('#arpInput').val('');
+}
